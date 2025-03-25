2e1822574612b93847a14293c3c23f4e9cfa70f8
Jude Woolls <jwoolls01@gmail.com>
2025-03-25 16:30:17 +0000

    display current selection of page in navbar

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,12 @@
-console.log('Welcome to WoollsWebsites.com!');
-
 window.addEventListener('resize', function() {
     window.innerWidth > 768 ? document.querySelector('#navbar').classList.remove('nav--mobile') : document.querySelector('#navbar').classList.add('nav--mobile');
     setUpNavbar(document.querySelector('#navbar'));
 });
 
 document.addEventListener('DOMContentLoaded', function() {
-        window.innerWidth > 768 ? document.querySelector('#navbar').classList.remove('nav--mobile') : document.querySelector('#navbar').classList.add('nav--mobile');
-        setUpNavbar(document.querySelector('#navbar'));
+    window.innerWidth > 768 ? document.querySelector('#navbar').classList.remove('nav--mobile') : document.querySelector('#navbar').classList.add('nav--mobile');
+    setUpNavbar(document.querySelector('#navbar'));
+    setActivePage();
 });
 
 const originalNav = `
@@ -15,10 +14,10 @@ const originalNav = `
         <nav>
             <h1 id="heading-title">Woolls Websites</h1>
             <ul>
-                <li><a href="#" class="active-page">Home</a></li>
-                <li><a href="#my-services-section">Services</a></li>
-                <li><a href="portfolio.html">Portfolio</a></li>
-                <li><a href="contact.html">Contact</a></li>
+                <li><a href="index.html" class="active-page myNavLinks">Home</a></li>
+                <li><a href="index.html#my-services-section" class="myNavLinks">Services</a></li>
+                <li><a href="portfolio.html" class="myNavLinks">Portfolio</a></li>
+                <li><a href="contact.html" class="myNavLinks">Contact</a></li>
             </ul>
         </nav>
     </header>`
@@ -35,22 +34,43 @@ function setUpNavbar(navbar) {
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a href="#" class="active-page">Home</a>
+          <a href="index.html" class="active-page myNavLinks">Home</a>
         </li>
         <li class="nav-item">
-          <a href="index.html#my-services-section">Services</a>
+          <a href="index.html#my-services-section" class="myNavLinks">Services</a>
         </li>
         <li class="nav-item">
-          <a href="portfolio.html">Portfolio</a>
+          <a href="portfolio.html" class="myNavLinks">Portfolio</a>
         </li>
         <li class="nav-item">
-          <a href="contact.html">Contact</a>
+          <a href="contact.html" class="myNavLinks">Contact</a>
         </li>
       </ul>
     </div>
   </div>
-</nav>`
+</nav>`;
     } else {
         navbar.outerHTML = originalNav;
     }
+    // Call setActivePage after the DOM has been updated
+    setTimeout(setActivePage, 0);
+}
+
+function setActivePage() {
+    const links = document.querySelectorAll('.myNavLinks');
+    const currentPath = window.location.pathname.split('/').pop();
+
+    links.forEach(link => {
+        const linkPath = link.getAttribute('href').split('/').pop();
+        if (linkPath === currentPath || (linkPath === '' && currentPath === 'index.html')) {
+            link.classList.add('active-page');
+        } else {
+            link.classList.remove('active-page');
+        }
+
+        link.addEventListener('click', function() {
+            links.forEach(link => link.classList.remove('active-page'));
+            this.classList.add('active-page');
+        });
+    });
 }
