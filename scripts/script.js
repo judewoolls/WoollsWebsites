7b24d9a06365680254a44c4f2879a53f585ad565
Jude Woolls <jwoolls01@gmail.com>
2025-04-25 00:14:34 +0100

    add transitions to contact cards

--- scripts/script.js
+++ scripts/script.js
@@ -16,3 +16,33 @@ document.addEventListener('DOMContentLoaded', () => {
         modal.classList.add('intro-modal-end');
     }, 1000);
 });
+
+const contactsObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        if (entry.target.dataset.contact === '1') {
+          entry.target.classList.add('contacts-end');
+          entry.target.classList.remove('contacts-start');
+        } else if (entry.target.dataset.contact === '2') {
+          entry.target.classList.add('contacts-end-2');
+          entry.target.classList.remove('contacts-start-2');
+        }
+      } else {
+        if (entry.target.dataset.contact === '1') {
+          entry.target.classList.remove('contacts-end');
+          entry.target.classList.add('contacts-start');
+        } else if (entry.target.dataset.contact === '2') {
+          entry.target.classList.remove('contacts-end-2');
+          entry.target.classList.add('contacts-start-2');
+        }
+      }
+  });
+});
+
+const contactsLeft = document.querySelectorAll('.contacts-start');
+contactsLeft.forEach((el) => contactsObserver.observe(el));
+
+const contactsRight = document.querySelectorAll('.contacts-start-2');
+contactsRight.forEach((el) => contactsObserver.observe(el));
+
+
