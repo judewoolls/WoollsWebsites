14939c846f9cce36dd16dedf0ee051595c0f11fb
Jude Woolls <jwoolls01@gmail.com>
2025-05-05 18:31:56 +0100

    add transition animations to the service and project cards

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+// this is the header animation
 const logoObserver = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
       if (entry.isIntersecting) {
@@ -25,8 +26,8 @@ const textObserver = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hide');
 hiddenElements.forEach((el) => textObserver.observe(el));
-// end of the text content animation
 
+// modal interaction
 const modal = document.querySelector('.intro-modal');
 document.addEventListener('DOMContentLoaded', () => {
     setTimeout(() => {
@@ -34,6 +35,33 @@ document.addEventListener('DOMContentLoaded', () => {
     }, 1000);
 });
 
+// service section animation
+const serviceObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        if (entry.target.dataset.service === '1') {
+          entry.target.classList.add('service-card-end');
+          entry.target.classList.remove('service-card-start');
+        } else if (entry.target.dataset.service === '2') {
+          entry.target.classList.add('service-card-end-2');
+          entry.target.classList.remove('service-card-start-2');
+        }
+      } else {
+        if (entry.target.dataset.service === '1') {
+          entry.target.classList.remove('service-card-end');
+          entry.target.classList.add('service-card-start');
+        } else if (entry.target.dataset.service === '2') {
+          entry.target.classList.remove('service-card-end-2');
+          entry.target.classList.add('service-card-start-2');
+        }
+      }
+  });
+});
+const services = document.querySelectorAll('.service-card');
+
+services.forEach((el) => serviceObserver.observe(el));
+
+// contact section animation
 const contactsObserver = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
       if (entry.isIntersecting) {
